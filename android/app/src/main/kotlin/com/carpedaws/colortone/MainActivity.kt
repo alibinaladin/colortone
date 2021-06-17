1ea189b9933b1d9baaa50f1769274a1405201a77
Muhammed Ali Bin Alavudeen <52092120+alibinaladin@users.noreply.github.com>
2021-06-17 10:29:12 +0530

    face_detection_completed

---
+++ android/app/src/main/kotlin/com/carpedaws/colortone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.carpedaws.colortone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/faceselection.dart
@@ -0,0 +1,11 @@
+class faceselection extends StatefulWidget {
+  @override
+  _faceselectionState createState() => _faceselectionState();
+}
+
+class _faceselectionState extends State<faceselection> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
